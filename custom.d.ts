8d4d5a786b0e1cd1eb49aa5828584ce52e1bd6f5
安忆 <i@anyi.in>
2026-09-24 15:33:17 +0800

    build: update dependencies

--- custom.d.ts
+++ custom.d.ts
@@ -1,6 +1,5 @@
 declare var Fancybox:
-	| { bind: (selector: string, options?: object) => void }
-	| undefined;
+	{ bind: (selector: string, options?: object) => void } | undefined;
 
 interface Window {
 	__domReadyScriptRan: boolean | undefined;
